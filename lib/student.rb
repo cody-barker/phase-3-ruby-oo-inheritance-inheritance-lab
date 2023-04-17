3b61bb6df8c9f7f08289a579d6767f81a34d7629
Cody <codybarker.or@gmail.com>
2023-04-17 12:07:15 -0700

    s

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,15 @@
-require_relative './user'
-
 class Student < User
+    attr_reader :knowledge
+
     def initialize(knowledge)
         @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
     end
 end
